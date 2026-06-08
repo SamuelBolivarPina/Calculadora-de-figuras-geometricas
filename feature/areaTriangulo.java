1cd8e0733ed76f8d70af2187f3ea2f5187c09e4b
Samuel Bolivar Piña <sbolivarp1@unicartagena.edu.co>
2026-06-08 17:35:35 -0500

    Corregido nextFloat por nextDouble

--- feature/areaTriangulo.java
+++ feature/areaTriangulo.java
@@ -3,12 +3,11 @@ import java.util.Scanner;
 public class areaTriangulo {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        float base, altura,area;
         System.out.println("Ingresar base");
-        base = sc.nextFloat();
+        double base = sc.nextDouble();
          System.out.println("Ingresar altura");
-          altura = sc.nextFloat();
-          area = base * altura / 2;
+        double altura = sc.nextDouble();
+          double area = base * altura / 2;
           System.out.println("El area del triangulo es: " + area);
           sc.close();
     }
