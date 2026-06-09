6aa49ca610e6c69077789edde4594c867301b5b5
Paolo Trujillo Artunduaga <ptrujilloa@unicartagena.edu.co>
2026-06-08 19:22:12 -0500

    Declaración de variables

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,18 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import  java.util.Scanner;
 
 public static void main(String[] args) {
+    System .out.println("____CALCULADORA DEL AREA DE UN CIRCULO____\n");
+    System.out.println("El radio del circulo es: ");
     areaCirculo();
 }
 
 public static void areaCirculo(){
-    System.out.println("Area Circulo");
+    System .out.println("Ingrese el radio del circulo: ");
+    Scanner scanner = new Scanner(System.in);
+    //Variables:
+    double radio = scanner.nextDouble();
+    double area = Math.PI * radio * radio;
+
 }
