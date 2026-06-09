972dca8769b040a7ea50cbfbeb88d1341ead383e
Paolo Trujillo Artunduaga <ptrujilloa@unicartagena.edu.co>
2026-06-08 19:36:00 -0500

    Finalizacion del codigo

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,14 @@ import  java.util.Scanner;
 
 public static void main(String[] args) {
     System .out.println("____CALCULADORA DEL AREA DE UN CIRCULO____\n");
-    System.out.println("El radio del circulo es: ");
-    areaCirculo();
-}
-
-public static void areaCirculo(){
     System .out.println("Ingrese el radio del circulo: ");
     Scanner scanner = new Scanner(System.in);
     //Variables:
     double radio = scanner.nextDouble();
-    double area = Math.PI * radio * radio;
+    areaCirculo(radio);
+}
 
+public static void areaCirculo(double radio){
+    double area = Math.PI * radio * radio;
+    System.out.println("\nEl area del circulo cuyo radio es "+radio+" es: "+area);
 }
