387f4095d376c2d6ccaacd164e05251b630d9b2c
Samuel Bolivar Piña <sbolivarp1@unicartagena.edu.co>
2026-06-08 23:53:32 -0500

    se le quito el sc close() y se le agrego el return al area

--- feature/areaTriangulo.java
+++ feature/areaTriangulo.java
@@ -1,13 +1,30 @@
 package feature;
+
 import java.util.Scanner;
+
 public class areaTriangulo {
-    public static void main(String[] args){
+
+    
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
         System.out.println("Ingresar base");
         double base = sc.nextDouble();
-         System.out.println("Ingresar altura");
+        
+        System.out.println("Ingresar altura");
         double altura = sc.nextDouble();
-          double area = base * altura / 2;
-          System.out.println("El area del triangulo es: " + area);
+        
+    
+        double resultado = calcularArea(base, altura);
+        
+        System.out.println("El area del triangulo es: " + resultado);
+        
+        sc.close();
+    }
+
+    
+    public static double calcularArea(double base, double altura) {
+        double area = (base * altura) / 2;
+        return area;
     }
 }
