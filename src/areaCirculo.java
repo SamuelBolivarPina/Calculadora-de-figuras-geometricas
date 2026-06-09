0c6dd70354f6aaf0203a980df93289c0f012da9e
Paolo Trujillo Artunduaga <ptrujilloa@unicartagena.edu.co>
2026-06-08 20:58:33 -0500

    Creacion de clase AreaCirculo

--- src/areaCirculo.java
+++ src/areaCirculo.java
@@ -1,17 +1,21 @@
 import  java.util.Scanner;
 
-public static void main(String[] args) {
-    System .out.println("____CALCULADORA DEL AREA DE UN CIRCULO____\n");
-    System .out.println("Ingrese el radio del circulo: ");
-    Scanner scanner = new Scanner(System.in);
-    //Variables:
-    double radio = scanner.nextDouble();
-    //Llamado de la funcion
-    areaCirculo(radio);
+class AreaCirculo{
+
+    public static void main(String[] args) {
+        System .out.println("____CALCULADORA DEL AREA DE UN CIRCULO____\n");
+        System .out.println("Ingrese el radio del circulo: ");
+        Scanner scanner = new Scanner(System.in);
+        //Variables:
+        double radio = scanner.nextDouble();
+        //Llamado de la funcion
+        areaCirculo(radio);
+    }
+
+    //Metodo para calcular el area
+    public static void areaCirculo(double radio){
+        double area = Math.PI * radio * radio;
+        System.out.println("\nEl area del circulo cuyo radio es "+radio+" es: "+area);
+    }
 }
 
-//Metodo para calcular el area
-public static void areaCirculo(double radio){
-    double area = Math.PI * radio * radio;
-    System.out.println("\nEl area del circulo cuyo radio es "+radio+" es: "+area);
-}
